93aa20f7f66a2a5b106527765413cfebe3378e7c
logant08 <logantillman@ymail.com>
2021-11-08 22:06:30 -0500

    auto: Import generated Spring project

---
+++ pw-backend/src/main/java/ltillma4/pwbackend/PwBackendApplication.java
@@ -0,0 +1,13 @@
+package ltillma4.pwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwBackendApplication.class, args);
+	}
+
+}
---
+++ pw-backend/src/test/java/ltillma4/pwbackend/PwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ltillma4.pwbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
