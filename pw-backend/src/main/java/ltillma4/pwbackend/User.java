6f8081ea6c15c62e78d753dac751c219a2bf28e8
logant08 <logantillman@ymail.com>
2021-11-09 00:11:50 -0500

    feat: Implement User model

---
+++ pw-backend/src/main/java/ltillma4/pwbackend/User.java
@@ -0,0 +1,24 @@
+package ltillma4.pwbackend;
+
+public class User {
+
+    private final String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
